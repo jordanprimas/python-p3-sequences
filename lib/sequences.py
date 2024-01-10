606ea7ebacc79ca1262a1186d5899c22b33b3bad
Jordan Primas <jorprimas20@gmail.com>
2024-01-09 20:28:25 -0800

    assignment progress

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,15 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    if length == 0:
+        return []
+    if length == 1:
+        return [0]
+    else:
+        fibonacci = [0, 1]
+    while len(fibonacci) < length:
+        next_number = fibonacci[-1] + fibonacci[-2]
+        fibonacci.append(next_number)
+    return fibonacci
+
+    
